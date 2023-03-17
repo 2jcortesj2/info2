66ec2c4bb82184c3e731e54ea784f612bfb674d9
2jcortesj2 <jjose.cortes@udea.edu.co>
2023-03-17 11:15:37 -0500

    ordené los archivos

--- Ejercicio de panda.py
+++ Ejercicio de panda.py
@@ -1,12 +1,14 @@
 import pandas as pd 
 import numpy as np
+import os
+import glob
 
 s = pd.Series(np.random.randn(5), index=["a","b","c","d","e"])
 s.index
 print(s)
 print(s.index)
 
-# por defecto ordena los números
+# por defecto ordena los indices
 d = {'b':1, 'a':0, 'c':2}
 s = pd.Series(d)
 print(s)
@@ -15,3 +17,8 @@ print(s)
 d = {'b':1, 'a':0, 'c':2}
 s = pd.Series(d, index=["b","c","d","a"])
 print(s)
+
+current = os.getcwd()
+file = glob.glob(current+'/*.csv')
+print(file)
+mmse = pd.read_csv(file[0], sep=';')
