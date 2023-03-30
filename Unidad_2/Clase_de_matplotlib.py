262d14231d863f97aa6604f4686b4fb5f04cd073
2jcortesj2 <jjose.cortes@udea.edu.co>
2023-03-30 13:36:57 -0500

    Update

--- Unidad_2/Clase_de_matplotlib.py
+++ Unidad_2/Clase_de_matplotlib.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 2*np.pi, 200)
@@ -6,4 +6,4 @@ y = np.sin(x)
 
 fig, ax = plt.subplots()
 ax.plot(x, y)
-plt.show
+plt.show()
